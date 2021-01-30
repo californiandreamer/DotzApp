65f091b5466109dfc8b643dfffb9e802b4feb264
californiandreamer <sydyahanazar777@gmail.com>
2021-01-30 16:59:28 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello! I am DOTZ app</Text>
+    </View>
+  );
+};
+
+export default App;
